96e7511eb649ebd138c47c105a278e1afce03c1e
Arturas <mixmln14@gmail.com>
2023-04-18 19:55:55 +0300

    last changes for today

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../scss/hero.scss';
 
 function Hero() {
+	const [deviceWidth, setDeviceWidth] = useState(0);
+
+	useEffect(() => {
+		setDeviceWidth(window.innerWidth);
+	}, []);
+
 	return (
-		<div className='hero-section'>
-			<h1 className='text'>
-				web, soft and app design, development, hosting, maintenance & graphic
-				design affairs.
-			</h1>
-			{/* <h1 className='text reveal-text'>web, soft and</h1>
-			<h2 className='text reveal-text'>app design,</h2>
-			<h3 className='text reveal-text'>development,</h3>
-			<h4 className='text reveal-text'>hosting,</h4>
-			<h5 className='text reveal-text'>maintenance</h5>
-			<h6 className='text reveal-text'>& graphic</h6>
-			<p className='text reveal-text'>design affairs.</p> */}
-		</div>
+		<>
+			{deviceWidth <= 1024 ? (
+				<div className='hero-section'>
+					<h1 className='text reveal-text'>web, soft and</h1>
+					<h2 className='text reveal-text'>app design,</h2>
+					<h3 className='text reveal-text'>development,</h3>
+					<h4 className='text reveal-text'>hosting,</h4>
+					<h5 className='text reveal-text'>maintenance</h5>
+					<h6 className='text reveal-text'>& graphic</h6>
+					<p className='text reveal-text'>design affairs.</p>
+				</div>
+			) : (
+				<div className='hero-section'>
+					<h1 className='text reveal-text'>web, soft and app</h1>
+					<h2 className='text reveal-text'> design, development,</h2>
+					<h3 className='text reveal-text'>hosting, maintenance &</h3>
+					<h4 className='text reveal-text'>graphic design affairs.</h4>
+				</div>
+			)}
+		</>
 	);
 }
 
