e51916e24d9cd72fd0258a8bcafb0fd4935a32bb
Arturas <mixmln14@gmail.com>
2023-04-18 17:37:15 +0300

    overfloww

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,13 +4,17 @@ import '../scss/hero.scss';
 function Hero() {
 	return (
 		<div className='hero-section'>
-			<h1 className='text reveal-text'>web, soft and</h1>
+			<h1 className='text'>
+				web, soft and app design, development, hosting, maintenance & graphic
+				design affairs.
+			</h1>
+			{/* <h1 className='text reveal-text'>web, soft and</h1>
 			<h2 className='text reveal-text'>app design,</h2>
 			<h3 className='text reveal-text'>development,</h3>
 			<h4 className='text reveal-text'>hosting,</h4>
 			<h5 className='text reveal-text'>maintenance</h5>
 			<h6 className='text reveal-text'>& graphic</h6>
-			<p className='text reveal-text'>design affairs.</p>
+			<p className='text reveal-text'>design affairs.</p> */}
 		</div>
 	);
 }
